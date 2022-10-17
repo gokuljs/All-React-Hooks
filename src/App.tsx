e66e7e52d836a936cee567b69c3573c82516f6af
Gokul Js <gokuljs029@gmail.com>
2022-10-17 20:14:12 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useRef } from "react";
+
+// useReducer() hook
+
+// standard syntax for creating a useStateHook()
+// a variable then a dispatch function which is called when ever we want to change something;
+// it takes in two arguments reducer function and initial states
+// const [state, dispatch] = useReducer(reducer, { count: 0, showText: false });
+
+// {
+//   /* {state.showText && <p>{state?.count}</p>} */
+// }
+
+// {
+//   /* <button
+//         onClick={() => {
+//           dispatch({ type: "INCREMENT" });
+//           dispatch({ type: "toggleShowText" });
+//         }}
+//       >
+//         submit
+//       </button> */
+// }
+
+// reducer will have two arguments is state and action
+// const reducer = (state: any, action: any) => {
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return { count: state.count + 1, showText: state.showText };
+//     case "toggleShowText":
+//       return { count: state.count, showText: !state.showText };
+//     default:
+//       return state;
+//   }
+// };
+
+// useRef hook
+
+// mainly used for dom manipulations
+// const inputRef = useRef<any>(null);
+// const handleClick = () => {
+//   // means we always access the current value
+//   console.log(inputRef?.current?.focus(), "ssss");
+// };
+
+{
+  /* <input ref={inputRef} type="text" placeholder="Ex..." />
+<button onClick={handleClick}>focus</button> */
+}
 
 function App() {
+  // mainly used for dom manipulations
+  const inputRef = useRef<any>(null);
+  const handleClick = () => {
+    // means we always access the current value
+    console.log(inputRef?.current?.focus(), "ssss");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ textAlign: "center", padding: "3rem" }}>
+      <input ref={inputRef} type="text" placeholder="Ex..." />
+      <button onClick={handleClick}>focus</button>
     </div>
   );
 }
